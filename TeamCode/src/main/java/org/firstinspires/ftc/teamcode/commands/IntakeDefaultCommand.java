0f6444382e44a80c0f691d2ccac88ce721ba855f
GreenMachineStudios <116412610+GreenMachineStudios@users.noreply.github.com>
2025-05-24 14:18:16 -0700

    idk what im doing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/IntakeDefaultCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/IntakeDefaultCommand.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.commands;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.CommandBase;
 
 import org.firstinspires.ftc.teamcode.subsystems.ExtendoSubsystem;
@@ -8,6 +9,7 @@ import org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem;
 
 import java.util.function.BooleanSupplier;
 
+@Config
 public class IntakeDefaultCommand extends CommandBase {
     private final IntakeSubsystem intake;
     private final ExtendoSubsystem extendo;
@@ -32,19 +34,15 @@ public class IntakeDefaultCommand extends CommandBase {
         else
             intake.intake();
 
-        switch (intake.readSensorColor()) {
-            case YELLOW:
-                led.setMode(LEDSubsystem.LEDMode.YELLOW_SAMPLE);
-                break;
-            case BLUE:
-                led.setMode(LEDSubsystem.LEDMode.BLUE_SAMPLE);
-                break;
-            case RED:
-                led.setMode(LEDSubsystem.LEDMode.RED_SAMPLE);
-                break;
-            default:
-                led.setMode(LEDSubsystem.LEDMode.NO_SAMPLE);
-                break;
-        }
+        IntakeSubsystem.Color color = intake.getColor();
+
+        if (color.equals(IntakeSubsystem.Color.YELLOW))
+            led.setMode(LEDSubsystem.LEDMode.YELLOW_SAMPLE);
+        else if (color.equals(IntakeSubsystem.Color.BLUE))
+            led.setMode(LEDSubsystem.LEDMode.BLUE_SAMPLE);
+        else if (color.equals(IntakeSubsystem.Color.RED))
+            led.setMode(LEDSubsystem.LEDMode.RED_SAMPLE);
+        else
+            led.setMode(LEDSubsystem.LEDMode.NO_SAMPLE);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -4,12 +4,12 @@ import static org.stealthrobotics.library.opmodes.StealthOpMode.telemetry;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.stealthrobotics.library.Alliance;
 import org.stealthrobotics.library.StealthSubsystem;
+
+import java.util.ArrayList;
 
 @Config
 public class IntakeSubsystem extends StealthSubsystem {
@@ -28,11 +28,21 @@ public class IntakeSubsystem extends StealthSubsystem {
 
     private final RevColorSensorV3 colorSensor;
 
-    public enum ColorList {
-        RED,
-        BLUE,
-        YELLOW,
-        BLACK
+    private final ArrayList<Color> recentColors = new ArrayList<>();
+    public static int RECENT_COLOR_HISTORY_COUNT = 10;
+
+    public enum Color {
+        RED(0),
+        BLUE(1),
+        YELLOW(2),
+        BLACK(3);
+
+        private int index;
+        Color(int index) {
+            this.index = index;
+        }
+
+        int getIndex() { return index; }
     }
 
     public IntakeSubsystem(HardwareMap hardwareMap) {
@@ -73,31 +83,67 @@ public class IntakeSubsystem extends StealthSubsystem {
         intakeServo.setPosition((speed + 1) / 2);
     }
 
-    public ColorList readSensorColor() {
+    private Color readSensorColor() {
         if (colorSensor.red() > RED_ACTIVATION || colorSensor.blue() > BLUE_ACTIVATION) {
             if (colorSensor.red() > colorSensor.blue() - RED_VS_BLUE_CONSTANT) {
                 if (colorSensor.red() > colorSensor.green() - RED_VS_GREEN_CONSTANT) {
-                    return ColorList.RED;
+                    return Color.RED;
                 }
                 else {
-                    return ColorList.YELLOW;
+                    return Color.YELLOW;
                 }
             }
             else {
-                return ColorList.BLUE;
+                return Color.BLUE;
             }
         }
         else {
-            return ColorList.BLACK;
+            return Color.BLACK;
+        }
+    }
+
+    private void addNewColor(Color c) {
+        if (recentColors.size() < RECENT_COLOR_HISTORY_COUNT) {
+            recentColors.add(c);
+        }
+        else {
+            recentColors.remove(0);
+            recentColors.add(c);
+        }
+    }
+
+    public Color getColor() {
+        if (recentColors.isEmpty())
+            return Color.BLACK;
+        else {
+            //Return the most frequent color
+            int[] freq = new int[4];
+            for (Color color : recentColors) {
+                freq[color.getIndex()]++;
+            }
+
+            int max = Integer.MIN_VALUE;
+            int bestIndex = -1;
+            for (int i = 0; i < 4; i++) {
+                if (freq[i] > max) {
+                    max = freq[i];
+                    bestIndex = i;
+                }
+            }
+
+            switch (bestIndex) {
+                case 0: return Color.RED;
+                case 1: return Color.BLUE;
+                case 2: return Color.YELLOW;
+                default: return Color.BLACK;
+            }
         }
     }
 
     @Override
     public void periodic() {
-        telemetry.addData("Detected Color", readSensorColor());
+        addNewColor(readSensorColor());
 
-//        telemetry.addData("R", colorSensor.red());
-//        telemetry.addData("G", colorSensor.green());
-//        telemetry.addData("B", colorSensor.blue());
+        telemetry.addData("Detected Color", getColor());
     }
 }
