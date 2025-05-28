11f80165202a4e85c3155a6d576daa8f97f6a3f0
GreenMachineStudios <116412610+GreenMachineStudios@users.noreply.github.com>
2025-05-28 02:10:01 -0700

    why won't it workkk???? nvm it works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SpecimenAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SpecimenAuto.java
@@ -3,63 +3,57 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
-import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
-import com.pedropathing.pathgen.BezierCurve;
 import com.pedropathing.pathgen.BezierLine;
-import com.pedropathing.pathgen.Path;
 import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 import org.firstinspires.ftc.teamcode.subsystems.ClawSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.ElevatorSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.ExtendoSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.MecanumSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.PedroFollowerSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.FollowerSubsystem;
 import org.stealthrobotics.library.opmodes.StealthOpMode;
 
 @Autonomous(name = "SpecimenAuto")
 public class SpecimenAuto extends StealthOpMode {
-    private Follower follower;
+    private FollowerSubsystem follower;
     private ElevatorSubsystem elevator;
     private ClawSubsystem claw;
     private ExtendoSubsystem extendo;
-
-    private PedroFollowerSubsystem pedro;
 
     private final Pose startPose = new Pose(8.25, 65);
     private final Pose scoringPose = new Pose(37.5, 65);
     private final Pose scoochPose = new Pose(37.5, 70);
     private final Pose pickupPose = new Pose(13.25, 36, Math.toRadians(180.01));
 
-    private PathChain scoreSpecimenInital, scoreSpecimen, pickupSpecimen;
+    private PathChain scoreSpecimenInitial, scoreSpecimen, pickupSpecimen;
 
     @Override
     public void initialize() {
-        follower = new Follower(hardwareMap, FConstants.class, LConstants.class);
+        follower = new FollowerSubsystem(hardwareMap);
+        follower.setStartPose(startPose);
+
         elevator = new ElevatorSubsystem(hardwareMap);
         claw = new ClawSubsystem(hardwareMap);
         extendo = new ExtendoSubsystem(hardwareMap);
-
-        pedro = new PedroFollowerSubsystem(follower);
 
         buildPaths();
     }
 
     public void buildPaths() {
-        scoreSpecimenInital = follower.pathBuilder()
+        scoreSpecimenInitial = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(startPose), new Point(scoringPose)))
-//                .setPathEndTimeoutConstraint(0)
+//                .setConstantHeadingInterpolation(startPose.getHeading())
+//                .setPathEndTimeoutConstraint(0) What does this do?
                 .build();
     }
 
     @Override
     public Command getAutoCommand() {
         return new SequentialCommandGroup(
-                pedro.followPath(scoreSpecimenInital, true)
+                new InstantCommand(() -> claw.setState(ClawSubsystem.ClawState.CLOSED)),
+                follower.followPath(scoreSpecimenInitial, true)
         );
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FollowerSubsystem.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitUntilCommand;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.PathBuilder;
+import com.pedropathing.pathgen.PathChain;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+import org.stealthrobotics.library.StealthSubsystem;
+
+public class FollowerSubsystem extends StealthSubsystem {
+    private final Follower follower;
+
+    public FollowerSubsystem(HardwareMap hardwareMap) {
+        follower = new Follower(hardwareMap, FConstants.class, LConstants.class);
+    }
+
+    public Command followPath(Path path, boolean holdPoint) {
+        return new SequentialCommandGroup(
+                new InstantCommand(() -> follower.followPath(path, holdPoint)),
+                new WaitUntilCommand(() -> !follower.isBusy())
+        );
+    }
+
+    public Command followPath(PathChain path, boolean holdPoint) {
+        return new SequentialCommandGroup(
+                new InstantCommand(() -> follower.followPath(path, holdPoint)),
+                new WaitUntilCommand(() -> !follower.isBusy())
+        );
+    }
+
+    public void setStartPose(Pose startPose) {
+        follower.setStartingPose(startPose);
+    }
+
+    public PathBuilder pathBuilder() {
+        return follower.pathBuilder();
+    }
+
+    @Override
+    public void periodic() {
+        follower.update();
+    }
+}
